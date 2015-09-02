e68b930e275a20bd372f7061f607bafb820779b2
Thomas Plummer <1thomas.plummer1@gmail.com>
2015-09-02 18:56:21 -0500

    Updated the 'gameCommands.lua' in the /readme.

--- readme/gameCommands.lua
+++ readme/gameCommands.lua
@@ -1,4 +1,4 @@
---[[ 
+﻿--[[ 
 
 	List of commands:
 		
@@ -7,19 +7,19 @@
 
 		MISSING:		man, info, switches•
 
-		dir [Path]
-		cd [Path] [target]
-		move [Source] [target]
-		run [executable] [destination]
-		cls
-		help
-		type [Path] filename
-		del [target]
-		mkdir [Path]
-		ren [Path] filename1 filename2
-		pause
-		mount [Type] [Path]
-		umount [Type] [Path]
+		dir [Path]				|	ls [Path]
+		cd [Path] [target]			|	cd [Path][Target]
+		move [Source] [target]			|	mv [Source] [Target]
+		run [executable] [destination]		|	[File].exe
+		cls					|	clear
+		help					|	help
+		type [Path] filename			|	cat [Path][Filename]
+		del [target]				|	rm [Path][Filename]
+		mkdir [Path]				|	mkdir [Path]
+		ren [Path] filename1 filename2		|	mv [old_Filename] [new_Filename]
+		pause					|	//NOT USED UNLESS CODING
+		mount [Type] [Path]			|	mount [obj to mount] [Path]
+		umount [Type] [Path]			|	umount [Path]
 
 		•switches would be tagged on after attack.exe and have a numerical value afterwards to indicate strength of attack and type of attack
 
