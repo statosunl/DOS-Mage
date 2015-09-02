554af4850a0f89beb6ca617709cfde1225148cdb
Thomas Plummer <1thomas.plummer1@gmail.com>
2015-09-02 18:39:14 -0500

    Brief Un-organized Addition
    This has everything that we need to start up the actual coding process.
    I'll organize it later.

---
+++ readme/DOS Mage writeup.lua
@@ -0,0 +1,17 @@
+--[[
+
+	Idea:	DOS Mage - Dungeons would be a floor based dungeon crawler where the player has access to an emulated command line.
+			The player has to navigate various directories (e.g. floors, self, chests) to get to the bottom of the dungeon.
+			As you go you can collect items dropped by enemies and manage your inventory and stats.
+			You can play as a variety of heros (I have art for mage, rogue, warrior, claric, warlock, ranger, and orc).
+			Characters are unlocked at floor milestone (e.g. floor 5, 10, 20, etc).
+			Your characters level up and you can mount skill points to different stats.
+
+	Using:	Love2d Engine and frameworks that go along with it.
+
+	Group:	Joel Williams, Jaino Vennet, Esteban Kott, Melec Rodriguez, Tom Plumber
+
+	Storyline:	An evil warlock has triumphed over the greatest hero in the land, computerizing his brain and locking in at the top of a dungeon.
+				Fight through hordes of darkness and defeat the evil warlock to free your mind and the world!
+
+]]--
---
+++ readme/The Dungeon Example/gameItemsStats.lua
@@ -0,0 +1,64 @@
+--[[ 
+
+	List of items:
+		
+		ITEMS NOTICE: These are gathered throughout the game and can be moved into a players inventory. Each item takes up space
+		
+		STATS NOTE:	The stats are: hp, armor, vit, str, air, wis
+					base stats are: 100 hp, 0 armor, 5 vit, 5 str, 5 air, 5 wis
+
+		health.potion 				4 KB	gives you 50 hp points
+		melee.potion				4 KB	gives you +20 str for 10 seconds
+		spell.potion 				4 KB	gives you +20 wis for 10 seconds
+		range.potion				4 KB	gives you +20 air for 10 seconds
+		vitality.potion				4 KB    gives you +20 health regen for 10 seconds | negates on hit
+		
+		sword.item 				2 KB	+5 str
+		shortsword.item 			4 KB	+10 str
+		longsword.item 				8 KB	+25 str
+		broadsword.item 			16 KB	+50 str
+		excalibur.item 				32 KB	+500 str
+		24833191f2e87dfdccff6b19d8178a9f	64 KB   +1000 str | +1000 true damage | melee immunity
+		
+		halfwand.item 				2 KB	+5 wis
+		wand.item 	 			4 KB	+10 wis
+		airstaff.item 				8 KB 	+25 wis
+		firestaff.item				8 KB	+50 wis
+		staffofgod.item 			16 KB	+500 wis
+		66c579059228655ab6824b4fa4fcd8fc	64 KB   +1000 wis | +1000 true damage | spell immunity
+		
+		shortbow.item				2 KB	+5 air
+		longbow.item				4 KB	+10 air
+		crossbow.item 				8 KB	+25 air
+		reflexbow.item 				16 KB	+50 air
+		compoundbow.item			32 KB	+500 air
+		b01682488daeefd3aa25642ef1cfd799	64 KB   +1000 air | +1000 true damage | ranged immunity
+		
+		leatherarmor.item 			2 KB	+5 armor
+		studdedleatherarmor.item 		4 KB	+10 armor
+		chainmailarmor.item 			8 KB	+25 armor
+		ironarmor.item 				16 KB	+50 armor
+		enchantedarmor.item 			32 KB	+500 armor
+		
+		bronzering.item 			2 KB	all stats +1
+		ironring.item 				4 KB	all stats +2
+		goldring.item 				8 KB	all stats +4
+		diamondring.item 			16 KB 	all stats +10
+		onering.item 				32 KB 	all stats +100
+		
+		poison.item				4 KB	+1 true damage | compounds
+		adrenaline.item				4 KB	100% hit chance | +10 all stats
+		greekfire.item				16 KB	100% hit all enemies | 5 true damage | compounds
+		ambrosia.item				32 KB	100% health heal | 100% armor heal | 100% durability heal
+		
+
+		rustydagger.item 			2 KB	all stats +1000
+		dagger.item				4 KB	+5 str | +5 air
+		dualdaggers.item			4 KB    +5 str | +10 air
+
+		brokenrune.item				2 KB	+5 wis | +5 vit
+		demonrune.item				4 KB	+10 wis | +10 vit
+		dragonrune.item				8 KB	+15 wis | +15 vit
+		hydrarune.item				16 KB	+20 wis | +20 vit
+
+]]--
