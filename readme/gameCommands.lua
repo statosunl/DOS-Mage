707fefbaff50576258220456c88dffcef8f8794d
statosunl <statosunl@gmail.com>
2015-09-02 22:42:07 -0500

    gameCommands.lua
    just editing the left side to look like the right.

--- readme/gameCommands.lua
+++ readme/gameCommands.lua
@@ -8,18 +8,18 @@
 		MISSING:		man, info, switches•
 
 		dir [Path]				|	ls [Path]
-		cd [Path] [target]			|	cd [Path][Target]
-		move [Source] [target]			|	mv [Source] [Target]
-		run [executable] [destination]		|	[File].exe
+		cd [Path][target]			|	cd [Path][Target]
+		move [Source][target]			|	mv [Source] [Target]
+		[file].exe				|	[File].exe
 		cls					|	clear
 		help					|	help
-		type [Path] filename			|	cat [Path][Filename]
-		del [target]				|	rm [Path][Filename]
+		type [Path][filename]			|	cat [Path][Filename]
+		del [Path][filename]			|	rm [Path][Filename]
 		mkdir [Path]				|	mkdir [Path]
-		ren [Path] filename1 filename2		|	mv [old_Filename] [new_Filename]
+		ren [Path][old_filename][new_filename]	|	mv [old_Filename][new_Filename]
 		pause					|	//NOT USED UNLESS CODING
-		mount [Type] [Path]			|	mount [obj to mount] [Path]
-		umount [Type] [Path]			|	umount [Path]
+		mount [obj to mount][Path]		|	mount [obj to mount][Path]
+		umount [Path]				|	umount [Path]
 
 		•switches would be tagged on after attack.exe and have a numerical value afterwards to indicate strength of attack and type of attack
 
